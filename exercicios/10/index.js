7016a49111c2da1e8de826880a9b0d66775db2c4
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2022-03-16 17:25:08 -0300

    feat: aula 06 - exercício programa com get e set

--- exercicios/10/index.js
+++ exercicios/10/index.js
@@ -1,10 +1,19 @@
 class Programa {
   #generosPermitidos
+  #generos
 
   constructor(duracao, generos) {
     this.#generosPermitidos = ["ação", "comédia", "suspense", "drama", "terror"]
     this.duracao = duracao
-    this.generos = generos.filter((genero) => {
+    this.generos = generos
+  }
+
+  get generos() {
+    return this.#generos
+  }
+
+  set generos(generos) {
+    this.#generos = generos.filter((genero) => {
       return this.#generosPermitidos.includes(genero)
     })
   }
