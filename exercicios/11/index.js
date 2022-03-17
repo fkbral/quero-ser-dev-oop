3fbbbe33fcdb1dc93f0831493a472c1e0284a2c5
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2022-03-17 15:56:03 -0300

    feat: aula 07 - exercício do Livro com validações (opcional)

--- exercicios/11/index.js
+++ exercicios/11/index.js
@@ -6,16 +6,38 @@ class Livro {
 }
 
 class Ebook extends Livro {
+  #numeroDeCapitulos
+
   constructor(autor, ano, numeroDePaginas, numeroDeCapitulos) {
     super(autor, ano)
     this.numeroDePaginas = numeroDePaginas
     this.numeroDeCapitulos = numeroDeCapitulos
   }
+
+  get numeroDeCapitulos() {
+    return this.#numeroDeCapitulos
+  }
+
+  set numeroDeCapitulos(numeroDeCapitulos) {
+    this.#numeroDeCapitulos = 
+      numeroDeCapitulos >= numeroDePaginas ? numeroDeCapitulos : numeroDePaginas
+  }
 }
 
 class AudioBook extends Livro {
+  #duracaoDoAudioEmMinutos
+
   constructor(autor, ano, duracaoDoAudioEmMinutos) {
     super(autor, ano)
     this.duracaoDoAudioEmMinutos = duracaoDoAudioEmMinutos
+  }
+
+  get duracaoDoAudioEmMinutos() {
+    return this.#duracaoDoAudioEmMinutos
+  }
+
+  set duracaoDoAudioEmMinutos(duracaoDoAudioEmMinutos) {
+    this.#duracaoDoAudioEmMinutos = 
+      duracaoDoAudioEmMinutos > 0 ? duracaoDoAudioEmMinutos : 1
   }
 }
