feefe4247233b82b3d07464184e0f3c244a8828e
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2022-03-17 14:36:19 -0300

    feat: aula 06 - exercício seriado com privado e get e set

--- exercicios/10/index.js
+++ exercicios/10/index.js
@@ -20,11 +20,40 @@ class Programa {
 }
 
 class Seriado extends Programa {
+  #anoDeFim
+  #numeroDeEpisodios
+
   constructor(duracao, generos, numeroDeEpisodios, anoDeInicio, anoDeFim) {
     super(duracao, generos)
-    this.numeroDeEpisodios = numeroDeEpisodios > 1 ? numeroDeEpisodios : 1
+    this.numeroDeEpisodios = numeroDeEpisodios
     this.anoDeInicio = anoDeInicio
-    this.anoDeFim = anoDeFim > anoDeInicio ? anoDeFim : anoDeInicio
+    this.anoDeFim = anoDeFim
+  }
+
+  get numeroDeEpisodios() {
+    return this.#numeroDeEpisodios
+  }
+
+  set numeroDeEpisodios(numeroDeEpisodios) {
+    this.#numeroDeEpisodios = numeroDeEpisodios > 1 ? numeroDeEpisodios : 1
+  }
+
+  get anoDeFim() {
+    return this.#anoDeFim
+  }
+
+  set anoDeFim(anoDeFim) {
+    this.#anoDeFim = anoDeFim > this.anoDeInicio ? anoDeFim : this.anoDeInicio
+  }
+}
+
+class Filme extends Programa {
+  constructor(duracao, generos) {
+    super(duracao, generos)
+    const minutosExcedentesDeHoras = this.duracao % 60
+    const horas = this.duracao - minutosExcedentesDeHoras
+
+    this.duracao = `${horas/60}h ${minutosExcedentesDeHoras}m`
   }
 }
 
@@ -32,5 +61,8 @@ const programaX = new Programa(50, ["suspense", "drama", "romance"])
 
 const theOffice = new Seriado(22, ["comédia", "sátira"], 120, 2005, 2012)
 
+const naoOlhePraCima = new Filme(128, ["comédia", "suspense", "drama"])
+
 console.log(programaX)
 console.log(theOffice)
+console.log(naoOlhePraCima)
